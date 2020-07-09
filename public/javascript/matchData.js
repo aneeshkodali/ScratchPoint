936d58ed89069445759dd24c8059fdf03edd4141
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-07-09 13:01:32 -0400

    Filter Shots Table based on Point Clicked

--- public/javascript/matchData.js
+++ public/javascript/matchData.js
@@ -1,12 +1,18 @@
-//const mongoose = require("mongoose");
-//const Point = require("../models/point");
-//const Shot = require("../models/shot");
 
+const shotTable = document.querySelectorAll("#shotTable tbody tr");
 
 const pointTableRows = document.querySelectorAll("#pointTable tbody tr");
 pointTableRows.forEach(row => {
     row.addEventListener("click", e => {
-        let rowID = e.target.parentNode.id;
-        console.log(rowID);
+        //let rowID = e.target.parentNode.id;
+        let parentRow = e.target.parentNode.querySelector("th").innerText;
+
+        shotTable.forEach(shot => {
+            shot.style.display = "table-row";
+            let shotPoint = shot.querySelector("td").innerText;
+            if (parentRow !== shotPoint) {
+                shot.style.display = "none";
+            }
+        })
     });
 });
