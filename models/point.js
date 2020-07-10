4678a8fdef5d5ab5efb28e7521614b09f9995043
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-07-10 09:17:01 -0400

    Update Seed Date to Reflect DB Schema (Virtual) Changes

--- models/point.js
+++ models/point.js
@@ -5,18 +5,18 @@ const opts = { toJSON: { virtuals: true } };
 
 const pointSchema = new mongoose.Schema({
     point: Number,
-    setScoreServer: Number,
-    setScoreReceiver: Number,
-    gameScoreServer: Number,
-    gameScoreReceiver: Number,
-    pointScoreServer: String,
-    pointScoreReceiver: String,
+    player1: String,
+    player2: String,
+    setScorePlayer1: Number,
+    setScorePlayer2: Number,
+    gameScorePlayer1: Number,
+    gameScorePlayer2: Number,
+    pointScorePlayer1: Number,
+    pointScorePlayer2: Number, 
     server: String,
-    receiver: String,
     rallyLength: Number,
-    result: String,
     winner: String,
-    loser: String,
+    result: String,
     shots: [{type: mongoose.Schema.Types.ObjectId, ref: 'Shot'}]
 }, opts);
 
@@ -24,45 +24,70 @@ const pointSchema = new mongoose.Schema({
 
 // VIRTUALS
 pointSchema.virtual("side").get(function() {
-    return (this.pointScoreServer+this.pointScoreReceiver) % 2 === 0 ? "deuce" : "ad";
-})
+    return (this.pointScorePlayer1 + this.pointScorePlayer2) % 2 === 0 ? "deuce" : "ad";
+});
+
+pointSchema.virtual("receiver").get(function() {
+    return this.server === this.player1 ? this.player2 : this.player1;
+});
+
+//pointSchema.virtual("winner").get(function() {
+//    if (["ace", "service winner", "winner"]) {
+//        return this.rallyLength % 2 === 0 ? this.receiver : this.server;
+//    } else {
+//        return this.rallyLength % 2 === 0 ? this.server : this.receiver;
+//    }
+//});
+
+pointSchema.virtual("loser").get(function() {
+    return this.winner === this.player1 ? this.player2 : this.player1;
+});
+
+pointSchema.virtual("setScoreServer").get(function() {
+    return this.server === this.player1 ? this.setScorePlayer1 : this.setScorePlayer2;
+});
+
+pointSchema.virtual("gameScoreServer").get(function() {
+    return this.server === this.player1 ? this.gameScorePlayer1 : this.gameScorePlayer2;
+});
+
+pointSchema.virtual("pointScoreServer").get(function() {
+    return this.server === this.player1 ? this.pointScorePlayer1 : this.pointScorePlayer2;
+});
+
+pointSchema.virtual("setScoreReceiver").get(function() {
+    return this.server === this.player1 ? this.setScorePlayer2 : this.setScorePlayer1;
+});
+
+pointSchema.virtual("gameScoreReceiver").get(function() {
+    return this.server === this.player1 ? this.gameScorePlayer2 : this.gameScorePlayer1;
+});
+
+pointSchema.virtual("pointScoreReceiver").get(function() {
+    return this.server === this.player1 ? this.pointScorePlayer2 : this.pointScorePlayer1;
+});
 
 pointSchema.virtual("setInMatch").get(function() {
-    return this.setScoreServer + this.setScoreServer + 1;
+    return this.setScorePlayer1 + this.setScorePlayer2 + 1;
 });
 
 pointSchema.virtual("gameInSet").get(function() {
-    return this.gameScoreServer + this.gameScoreServer + 1;
+    return this.gameScorePlayer1 + this.gameScorePlayer2 + 1;
 });
 
 pointSchema.virtual("setScore").
 get(function() {
     return `${this.setScoreServer}-${this.setScoreReceiver}`;
-}).
-set(function(score) {
-    const setScoreServer = score.split("-")[0];
-    const setScoreReceiver = score.split("-")[1];
-    this.set({setScoreServer, setScoreReceiver});
 });
 
 pointSchema.virtual("gameScore").
 get(function() {
     return `${this.gameScoreServer}-${this.gameScoreReceiver}`;
-}).
-set(function(score) {
-    const gameScoreServer = score.split("-")[0];
-    const gameScoreReceiver = score.split("-")[1];
-    this.set({gameScoreServer, gameScoreReceiver});
 });
 
 pointSchema.virtual("pointScore").
 get(function() {
     return `${this.pointScoreServer}-${this.pointScoreReceiver}`;
-}).
-set(function(score) {
-    const pointScoreServer = score.split("-")[0];
-    const pointScoreReceiver = score.split("-")[1];
-    this.set({pointScoreServer, pointScoreReceiver});
 });
 
 
