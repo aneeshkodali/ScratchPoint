aa326d8005049322ed30c01e54edff45bb8e6232
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-07-10 07:59:58 -0400

    Rewrote Functio to Filter Shot Rows on Point Row Click

--- public/javascript/matchData.js
+++ public/javascript/matchData.js
@@ -1,17 +1,17 @@
 
-const shotTable = document.querySelectorAll("#shotTable tbody tr");
+const shotTableRows = document.querySelectorAll("#shotTable tbody tr");
 
 const pointTableRows = document.querySelectorAll("#pointTable tbody tr");
-pointTableRows.forEach(row => {
-    row.addEventListener("click", e => {
+pointTableRows.forEach(pointRow => {
+    pointRow.addEventListener("click", e => {
         //let rowID = e.target.parentNode.id;
-        let parentRow = e.target.parentNode.querySelector("th").innerText;
+        let pointNum = e.target.parentNode.querySelector("th").innerText;
 
-        shotTable.forEach(shot => {
-            shot.style.display = "table-row";
-            let shotPoint = shot.querySelector("td").innerText;
-            if (parentRow !== shotPoint) {
-                shot.style.display = "none";
+        shotTableRows.forEach(shotRow => {
+            shotRow.style.display = "table-row";
+            let shotPointNum = shotRow.querySelector("td").innerText;
+            if (shotPointNum !== pointNum) {
+                shotRow.style.display = "none";
             }
         })
     });
